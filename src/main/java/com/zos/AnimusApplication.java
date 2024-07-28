722bc993f1bfa1fec44602d6269e331460b95790
s0oraj <hotmailsuraj@gmail.com>
2024-07-28 19:56:55 +0530

    Initial commit

---
+++ src/main/java/com/zos/AnimusApplication.java
@@ -0,0 +1,13 @@
+package com.zos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zos/InstagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class animusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
